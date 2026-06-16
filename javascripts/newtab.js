b9df22f62b88296318db8ade051b15430f5a0a3f
R9-Hu <renjiu96@gmail.com>
2026-06-16 15:46:50 -0400

    Deployed c0bbfc35 with MkDocs version: 1.6.1

--- javascripts/newtab.js
+++ javascripts/newtab.js
@@ -1,6 +1,8 @@
 (function () {
   function mark() {
-    document.querySelectorAll('.md-content a[href*="papers/"]').forEach(function (a) {
+    // Any link to an individual paper page (content tables/digests AND the
+    // 'Key papers' entries in the left nav). 'papers-list' is left as-is.
+    document.querySelectorAll('a[href*="papers/"]').forEach(function (a) {
       a.target = '_blank';
       a.rel = 'noopener';
     });
